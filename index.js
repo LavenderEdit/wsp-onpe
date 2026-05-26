bb782d04d519fa196a939723a49c3c3c61755b62
LavenderEdit <gercermagden@gmail.com>
2026-05-25 21:30:09 -0500

    Increase JSON and URL-encoded body size limits to 50mb

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import { router } from './routes.js';
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
 app.use('/', router);
